00a01809931471a21aa81ae941ee054422ed6c17
Daw <Daw@DESKTOP-8OVFM6T>
2023-05-04 18:10:58 +0200

    CONFIRMACION

---
+++ ProyectoGit/src/PaqueteRamal1/Rama1.java
@@ -0,0 +1,8 @@
+package PaqueteRamal1;
+
+public class Rama1 {
+    public static void main(String[] args) {
+        
+        System.out.println("¡Hola desde el método main de la clase Rama1!");
+    }
+}
