43c1d4112151bf2722ad98c5d8b1ccc279c690ca
Daw <Daw@DESKTOP-8OVFM6T>
2023-05-04 17:49:30 +0200

    TAREA GIT

---
+++ ProyectoGit/src/Alfombra/Ejercicio1.java
@@ -0,0 +1,20 @@
+package Alfombra;
+
+public class Ejercicio1 {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Practica entornitos");
+		System.out.println("Practica entornitos");
+		System.out.println("zelda breath of the wild");
+				
+				
+		}
+
+		
+
+}
+
+
